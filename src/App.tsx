e186fd23fa108c691afe5d6e9c3934e599e8a068
Kyle Carbonell <kylecarbonell13@gmail.com>
2024-09-28 00:13:00 -0700

    Testing for bugs

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [panoCount, setCount] = useState(0);
 
   useEffect(() => {
-    window.onbeforeunload = () => { }
+    // window.onbeforeunload = () => { }
     // window.addEventListener("beforeunload", (ev) => {
     //   ev.preventDefault();
     //   window.localStorage.setItem("Data", "")
